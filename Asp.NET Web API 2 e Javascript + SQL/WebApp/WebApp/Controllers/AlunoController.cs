f0d0f20d555e79c7df61ca4179ea6f6229087e49
Danilo Silva <danilo.silva@msn.com>
2021-05-10 17:57:53 -0300

    Acessando API

---
+++ Asp.NET Web API 2 e Javascript + SQL/WebApp/WebApp/Controllers/AlunoController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class AlunoController : ApiController
+    {
+        // GET: api/Aluno
+        public IEnumerable<Aluno> Get()
+        {
+            Aluno alunos = new Aluno();
+
+            return alunos.ListarAlunos();
+        }
+
+        // GET: api/Aluno/5
+        public Aluno Get(int id)
+        {
+            Aluno alunos = new Aluno();
+
+            return alunos.ListarAlunos().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/Aluno
+        public List<Aluno> Post(Aluno aluno)
+        {
+            Aluno _aluno = new Aluno();
+
+            _aluno.Inserir(aluno);
+
+            return _aluno.ListarAlunos();
+        }
+
+        // PUT: api/Aluno/5
+        public Aluno Put(int id, [FromBody]Aluno aluno)
+        {
+            Aluno _aluno = new Aluno();
+
+            return _aluno.Atualizar(id, aluno);
+        }
+
+        // DELETE: api/Aluno/5
+        public void Delete(int id)
+        {
+            Aluno _aluno = new Aluno();
+
+            _aluno.Excluir(id);
+        }
+    }
+}
