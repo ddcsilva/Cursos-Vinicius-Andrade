388842adbdcb22c7ba5f8ff01940521a487cd1a9
Danilo Silva <danilo.silva@msn.com>
2023-01-11 08:55:03 -0300

    Seção 3: Introdução ao EF Core

--- Asp.NET Web API e Angular + EF Core/EventosWeb/Back/src/EventosWeb.API/Controllers/EventoController.cs
+++ Asp.NET Web API e Angular + EF Core/EventosWeb/Back/src/EventosWeb.API/Controllers/EventoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EventosWeb.API.Data;
 using EventosWeb.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -12,41 +13,23 @@ namespace EventosWeb.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        private IEnumerable<Evento> _evento = new Evento[] {
-                new Evento() {
-                    EventoId = 1,
-                    Tema = "Angular 11 e .NET5",
-                    Local = "Belo Horizonte",
-                    Lote = "1º Lote",
-                    QuantidadePessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyy"),
-                    ImagemURL = "foto.png"
-                },
-                new Evento() {
-                    EventoId = 2,
-                    Tema = "Angular 14 e .NET6",
-                    Local = "São Paulo",
-                    Lote = "2º Lote",
-                    QuantidadePessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyy"),
-                    ImagemURL = "foto2.png"
-                }
-            };
+        private readonly DataContext _context;
 
-        public EventoController()
+        public EventoController(DataContext context)
         {
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
         public IEnumerable<Evento> GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.Where(evento => evento.EventoId == id);
         }
 
         [HttpPost]
---
+++ Asp.NET Web API e Angular + EF Core/EventosWeb/Back/src/EventosWeb.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using EventosWeb.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventosWeb.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
